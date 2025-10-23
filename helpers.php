d0636ffa2efb1c8c88c6216e47321c5dc3e61add
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-10-23 21:41:11 +0300

    created some helper functions to generate and load  partials and views easily

--- helpers.php
+++ helpers.php
@@ -9,3 +9,38 @@
 function basePath($path = ''){
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ *
+ * @Param  string $name
+ * @return void
+ */
+function loadView($name){
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if(file_exists($viewPath)){
+        require $viewPath;
+    }else{
+        echo "View '{$name} does not exist'";
+    }
+}
+
+
+
+/**
+ * Load a partial
+ *
+ * @Param  string $name
+ * @return void
+ */
+function loadPartial($name){
+    $partialPath =  basePath("views/partials/{$name}.php");
+
+
+    if(file_exists($partialPath)){
+        require $partialPath;
+    }else{
+        echo "View '{$name} does not exist'";
+    }
+}
