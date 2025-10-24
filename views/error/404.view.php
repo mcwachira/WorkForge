6d2b9f8075e54de8e32c9b84b2276cb1f2462710
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-10-24 17:57:23 +0300

    Created some basic controllers for the app

---
+++ views/error/404.view.php
@@ -0,0 +1,22 @@
+<?php  loadPartial('head'); ?>
+    <!-- Nav -->
+
+<?php  loadPartial('navbar'); ?>
+
+
+
+    <section>
+        <div class="container mx-auto p-4 mt-4">
+            <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3"> 404 Error</div>
+            <p class="text-center text-2xl mb-4">
+                This Page Does not exit
+            </p>
+            <a class="block text-center" href="/listings">Go Back To Listings</a>
+        </div>
+    </section>
+
+
+
+
+    <!--Footer -->
+<?php  loadPartial('footer'); ?>
