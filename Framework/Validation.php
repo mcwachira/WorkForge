56bb355bdf160a3cbf35f94a911224cf7bae7009
mcwachira <mcwachira@outlook.com>
2025-11-05 04:45:56 +0300

    Added functions to validate my data

---
+++ Framework/Validation.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace Framework;
+
+class Validation
+{
+
+    /**
+     * Validate a string
+     *
+     * @param string $value
+     * @param int $min
+     * @param float|int $max
+     * @return bool
+     *
+     *
+     */
+
+    public static function string(string $value, int $min = 1, float|int $max = INF): bool
+    {
+
+        if(is_string($value)) {
+            //remove whitespaces
+            $value = trim($value);
+            //check length
+            $length = strlen($value);
+            return $length >= $min && $length <= $max;
+        }
+
+        return false;
+    }
+
+    /**
+     * Validate Email Address
+     *
+     * @param string $value
+     * @return mixed
+     *
+     *
+     */
+
+    public static function email(string $value): mixed
+    {
+        $value = trim($value);
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+
+    /**
+     * Match value against another
+     *
+     * @param string $value1
+     * @param string $value2
+     * @return bool
+     *
+     *
+     */
+
+    public static function match(string $value1, string $value2): bool
+    {
+        $value1 = trim($value1);
+            $value2 = trim($value2);
+        return $value1 === $value2;
+    }
+}
